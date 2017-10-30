6faa25d1cb0837c8353458e70b84f45d4ca165e9
DK\dmsa <dmsa@sitecore.net>
2017-10-30 16:14:58 +0200

    Fix for issue #191147 - [EE] InsertPage dialog does not change item name in "Name" control when item is changed

---
+++ src/Sitecore.Support.191147/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.191147")]
+[assembly: AssemblyProduct("Sitecore.Support.191147")]
+[assembly: ComVisible(false)]
